06b29d776cd6967656b0b1007a2a21324a3f8d84
Allen Chang <allen.ty.chang@gmail.com>
2023-03-04 23:45:00 -0500

    save model every epoch

--- hw2/hw2_1/train.py
+++ hw2/hw2_1/train.py
@@ -148,6 +148,7 @@ def main():
         print(f"\nEpoch {epoch + 1}\n-------------------------------")
         start_time = time.time()
         train(model, epoch + 1, training_data_loader, loss_func)
+        torch.save(model, DATA_DIR / "s2vt_model.pkl")
         print("--- %s seconds ---" % (time.time() - start_time))
 
     print("\nTraining complete.\n")
